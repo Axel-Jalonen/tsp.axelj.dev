297572d83b789bc2d23c47a47a28c12e568435ed
MostLeVert <axeljalonen@gmail.com>
2024-01-11 15:46:39 -0800

    Fix behavior

--- index.js
+++ index.js
@@ -13,10 +13,19 @@ ctx.lineWidth = 10;
 
 var points = [];
 
-clearElement.addEventListener("click", () => {
+function clearCanvas() {
     points = [];
     ctx.clearRect(0, 0, c.width, c.height);
-})
+}
+function refreshCanvas() {
+    ctx.clearRect(0, 0, c.width, c.height);
+    points.forEach((point) => {
+        ctx.fillStyle = "black";
+        ctx.fillRect(point[0] - RECT_OFFSET, point[1] - RECT_OFFSET, RECT_SIZE, RECT_SIZE);
+    })
+}
+
+clearElement.addEventListener("click", clearCanvas);
 
 c.addEventListener("click", (e) => {
     var x = e.clientX * cssScaleX;
@@ -25,6 +34,7 @@ c.addEventListener("click", (e) => {
     ctx.fillRect(x - RECT_OFFSET, y - RECT_OFFSET, RECT_SIZE, RECT_SIZE);
     points.push([x, y]);
     const entry = getRandPoint();
+    refreshCanvas();
     solve(entry, points);
 })
 
@@ -37,7 +47,6 @@ function solve(currentPoint, pointsCpy) {
     if (pointsCpy.length === 0) {
         return;
     }
-
     var distance = Infinity;
     var bestPoint = null;
 
@@ -46,13 +55,14 @@ function solve(currentPoint, pointsCpy) {
         if ((distanceNew < distance) && !(point[0] === currentPoint[0] && point[1] === currentPoint[1])) {
             distance = distanceNew;
             bestPoint = { coords: point, distance: distance };
-            ctx.beginPath();
-            ctx.moveTo(currentPoint[0], currentPoint[1]);
-            ctx.lineTo(point[0], point[1]);
-            ctx.stroke();
-            ctx.closePath();
         }
     })
+
+    ctx.beginPath();
+    ctx.moveTo(currentPoint[0], currentPoint[1]);
+    ctx.lineTo(bestPoint.coords[0], bestPoint.coords[1]);
+    ctx.stroke();
+    ctx.closePath();
 
     pointsCpy = pointsCpy.filter(point => !(point[0] === bestPoint.coords[0] && point[1] === bestPoint.coords[1]));
     console.log(pointsCpy);
