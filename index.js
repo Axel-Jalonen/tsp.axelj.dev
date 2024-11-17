6e3d3f85774aac877001dae09f20dbb84d8cce21
Axel Jalonen <axeljalonen@gmail.com>
2024-11-17 13:14:45 -0800

    Use remote favicon

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ const RECT_OFFSET = RECT_SIZE / 2;
 ctx.lineWidth = 10;
 
 var options = {
-    algorithm: "nn",
-    optimize: false,
-    showEdges: true,
+  algorithm: "nn",
+  optimize: false,
+  showEdges: true,
 };
 
 var points = [];
@@ -31,66 +31,66 @@ var totalDistance = 0;
 // BEGIN: Event listeners
 
 function updateAlgorithmSelectedUI() {
-    const bnbButton = document.getElementById("bnb-button");
-    const nnButton = document.getElementById("nn-button");
+  const bnbButton = document.getElementById("bnb-button");
+  const nnButton = document.getElementById("nn-button");
 
-    if (options.algorithm === "bnb") {
-        bnbButton.children[0].textContent = "\u25CF"; // Selected
-        nnButton.children[0].textContent = "_"; // Not selected
-    } else {
-        bnbButton.children[0].textContent = "_"; // Not selected
-        nnButton.children[0].textContent = "\u25CF"; // Selected
-    }
+  if (options.algorithm === "bnb") {
+    bnbButton.children[0].textContent = "\u25CF"; // Selected
+    nnButton.children[0].textContent = "_"; // Not selected
+  } else {
+    bnbButton.children[0].textContent = "_"; // Not selected
+    nnButton.children[0].textContent = "\u25CF"; // Selected
+  }
 }
 
 function updateToggleUI(obj, prop, e) {
-    if (obj[prop]) {
-        e.target.children[0].textContent = "\u25CF";
-    } else {
-        e.target.children[0].textContent = "_";
-    }
+  if (obj[prop]) {
+    e.target.children[0].textContent = "\u25CF";
+  } else {
+    e.target.children[0].textContent = "_";
+  }
 }
 
 document.querySelectorAll("[data-algorithm]").forEach((button) => {
-    button.addEventListener("click", (e) => {
-        options.algorithm = e.target.getAttribute("data-algorithm");
-        updateAlgorithmSelectedUI();
-        enterWithRandom();
-    });
+  button.addEventListener("click", (e) => {
+    options.algorithm = e.target.getAttribute("data-algorithm");
+    updateAlgorithmSelectedUI();
+    enterWithRandom();
+  });
 });
 
 document.querySelector("[data-optimize]").addEventListener("click", (e) => {
-    options.optimize = !options.optimize;
-    updateToggleUI(options, "optimize", e);
-    refreshCanvas();
-    optimizeNn();
+  options.optimize = !options.optimize;
+  updateToggleUI(options, "optimize", e);
+  refreshCanvas();
+  optimizeNn();
 });
 
 document.querySelector("[data-edges]").addEventListener("click", (e) => {
-    options.showEdges = !options.showEdges;
-    updateToggleUI(options, "showEdges", e);
-    refreshCanvas();
-    enterWithRandom();
+  options.showEdges = !options.showEdges;
+  updateToggleUI(options, "showEdges", e);
+  refreshCanvas();
+  enterWithRandom();
 });
 
 document.getElementById("clear-button").addEventListener("click", clearCanvas);
 
 c.addEventListener("click", (e) => {
-    const x = e.clientX * cssScaleX;
-    const y = e.clientY * cssScaleY;
-    ctx.fillStyle = "black";
-    ctx.fillRect(x - RECT_OFFSET, y - RECT_OFFSET, RECT_SIZE, RECT_SIZE);
-    points.push([x, y]);
-    if (options.showEdges) {
-        if (options.optimize && options.algorithm === "nn") {
-            console.log("Running with optimization");
-            optimizeNn();
-        } else {
-            console.log("Running without optimization");
-            refreshCanvas();
-            enterWithRandom();
-        }
+  const x = e.clientX * cssScaleX;
+  const y = e.clientY * cssScaleY;
+  ctx.fillStyle = "black";
+  ctx.fillRect(x - RECT_OFFSET, y - RECT_OFFSET, RECT_SIZE, RECT_SIZE);
+  points.push([x, y]);
+  if (options.showEdges) {
+    if (options.optimize && options.algorithm === "nn") {
+      console.log("Running with optimization");
+      optimizeNn();
+    } else {
+      console.log("Running without optimization");
+      refreshCanvas();
+      enterWithRandom();
     }
+  }
 });
 
 // END: Event listeners
@@ -98,134 +98,133 @@ c.addEventListener("click", (e) => {
 // BEGIN: Helper functions
 
 function drawStatus(message) {
-    if (message) {
-        statusElement.textContent = message;
-    } else {
-        statusElement.textContent = "Click anywhere to start!";
-    }
+  if (message) {
+    statusElement.textContent = message;
+  } else {
+    statusElement.textContent = "Click anywhere to start!";
+  }
 }
 
 function drawAlgorithm(message) {
-    if (message) {
-        algorithmStatusElement.children[0].textContent = message;
-    } else {
-        algorithmStatusElement.children[0].textContent =
-            "Click anywhere to start!";
-    }
+  if (message) {
+    algorithmStatusElement.children[0].textContent = message;
+  } else {
+    algorithmStatusElement.children[0].textContent = "Click anywhere to start!";
+  }
 }
 
 function clearCanvas() {
-    points = [];
-    totalDistance = 0;
-    drawStatus();
-    ctx.clearRect(0, 0, c.width, c.height);
+  points = [];
+  totalDistance = 0;
+  drawStatus();
+  ctx.clearRect(0, 0, c.width, c.height);
 }
 
 function refreshCanvas() {
-    totalDistance = 0;
-    ctx.clearRect(0, 0, c.width, c.height);
-    points.forEach((point) => {
-        ctx.fillStyle = "black";
-        ctx.fillRect(
-            point[0] - RECT_OFFSET,
-            point[1] - RECT_OFFSET,
-            RECT_SIZE,
-            RECT_SIZE
-        );
-    });
+  totalDistance = 0;
+  ctx.clearRect(0, 0, c.width, c.height);
+  points.forEach((point) => {
+    ctx.fillStyle = "black";
+    ctx.fillRect(
+      point[0] - RECT_OFFSET,
+      point[1] - RECT_OFFSET,
+      RECT_SIZE,
+      RECT_SIZE
+    );
+  });
 }
 
 function optimizeNn() {
-    let bestDistance = Infinity;
-    let bestPoint;
-    points.forEach((point) => {
-        entry(point, points);
-        if (totalDistance < bestDistance) {
-            bestDistance = totalDistance;
-            bestPoint = point;
-        }
-        refreshCanvas();
-    });
-    entry(bestPoint, points);
+  let bestDistance = Infinity;
+  let bestPoint;
+  points.forEach((point) => {
+    entry(point, points);
+    if (totalDistance < bestDistance) {
+      bestDistance = totalDistance;
+      bestPoint = point;
+    }
+    refreshCanvas();
+  });
+  entry(bestPoint, points);
 }
 
 function getRandPoint() {
-    const index = Math.floor(Math.random() * points.length);
-    return points[index];
+  const index = Math.floor(Math.random() * points.length);
+  return points[index];
 }
 
 function enterWithRandom() {
-    if (options.optimize) {
-        optimizeNn();
-    } else {
-        entry(getRandPoint(), points);
-    }
+  if (options.optimize) {
+    optimizeNn();
+  } else {
+    entry(getRandPoint(), points);
+  }
 }
 
 // END: Helper functions
 
 function entry(currentPoint, pointsCpy) {
-    if (options.algorithm === "bnb") {
-        drawAlgorithm("Branch and Bound (exact) (NOT IMPLEMENTED)");
-        drawStatus("Coming soon.");
-        refreshCanvas();
-        throw new Error("Not implemented");
-        branchAndBound(currentPoint, pointsCpy);
-        return;
-    } else if (options.algorithm === "nn") {
-        drawAlgorithm("Nearest Neighbors (heuristic, greedy)");
-        drawStatus();
-        console.log("Running nearest neighbors");
-        nearestNeighbors(currentPoint, pointsCpy);
-    }
+  if (options.algorithm === "bnb") {
+    drawAlgorithm("Branch and Bound (exact) (NOT IMPLEMENTED)");
+    drawStatus("Coming soon.");
+    refreshCanvas();
+    throw new Error("Not implemented");
+    branchAndBound(currentPoint, pointsCpy);
+    return;
+  } else if (options.algorithm === "nn") {
+    drawAlgorithm("Nearest Neighbors (heuristic, greedy)");
+    drawStatus();
+    console.log("Running nearest neighbors");
+    nearestNeighbors(currentPoint, pointsCpy);
+  }
 }
 
 // ALGORITHMS
 
 function branchAndBound(currentPoint, pointsCpy) {
-    // TODO
+  // TODO
 }
 
 function nearestNeighbors(currentPoint, pointsCpy) {
-    if (pointsCpy.length === 0) {
-        return;
+  if (pointsCpy.length === 0) {
+    return;
+  }
+
+  var distance = Infinity;
+  var bestPoint;
+
+  pointsCpy.forEach((point) => {
+    if (!(point[0] === currentPoint[0] && point[1] === currentPoint[1])) {
+      const distanceNew = Math.sqrt(
+        Math.pow(currentPoint[0] - point[0], 2) +
+          Math.pow(currentPoint[1] - point[1], 2)
+      );
+      if (distanceNew < distance) {
+        distance = distanceNew;
+        bestPoint = point;
+      }
     }
+  });
 
-    var distance = Infinity;
-    var bestPoint;
+  totalDistance += distance;
+  if (totalDistance !== Infinity) {
+    drawStatus(`Total distance: ${totalDistance.toFixed(2)}`);
+  }
 
-    pointsCpy.forEach((point) => {
-        if (!(point[0] === currentPoint[0] && point[1] === currentPoint[1])) {
-            const distanceNew = Math.sqrt(
-                Math.pow(currentPoint[0] - point[0], 2) +
-                    Math.pow(currentPoint[1] - point[1], 2)
-            );
-            if (distanceNew < distance) {
-                distance = distanceNew;
-                bestPoint = point;
-            }
-        }
-    });
+  if (options.showEdges) {
+    ctx.beginPath();
+    ctx.moveTo(currentPoint[0], currentPoint[1]);
+    ctx.lineTo(bestPoint[0], bestPoint[1]);
+    ctx.stroke();
+    ctx.closePath();
+  }
 
-    totalDistance += distance;
-    if (totalDistance !== Infinity) {
-        drawStatus(`Total distance: ${totalDistance.toFixed(2)}`);
-    }
+  // We shouldn't use filter here
+  pointsCpy = pointsCpy.filter(
+    (point) =>
+      !(point[0] === bestPoint[0] && point[1] === bestPoint[1]) &&
+      !(point[0] === currentPoint[0] && point[1] === currentPoint[1])
+  );
 
-    if (options.showEdges) {
-        ctx.beginPath();
-        ctx.moveTo(currentPoint[0], currentPoint[1]);
-        ctx.lineTo(bestPoint[0], bestPoint[1]);
-        ctx.stroke();
-        ctx.closePath();
-    }
-
-    // We shouldn't use filter here
-    pointsCpy = pointsCpy.filter(
-        (point) =>
-            !(point[0] === bestPoint[0] && point[1] === bestPoint[1]) &&
-            !(point[0] === currentPoint[0] && point[1] === currentPoint[1])
-    );
-
-    nearestNeighbors(bestPoint, pointsCpy);
+  nearestNeighbors(bestPoint, pointsCpy);
 }
